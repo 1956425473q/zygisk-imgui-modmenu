a2d1ccdb3742798b22ff40635f13ce35a60743a7
c4005 <125218098+1956425473q@users.noreply.github.com>
2024-12-27 22:15:32 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -2,7 +2,7 @@
 #define ZYGISK_IMGUI_MODMENU_GAME_H
 
 // TODO: change this
-#define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define TargetLibName "libyuanshen.so"
+#define GamePackageName "com.miHoYo.Yuanshen"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
